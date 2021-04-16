9450f2a0782966a1c118ae8dfb62c6f76ae21159
Blayne Chard <blayne@chard.com>
2021-04-16 13:12:31 +1200

    refactor: apply eslint

--- src/commands/manifest.ts
+++ src/commands/manifest.ts
@@ -10,7 +10,7 @@ async function* parseDirectory(basePath: string, currentPath: string[]): AsyncGe
   const files = await fs.readdir(currentFolder, { withFileTypes: true });
   for (const file of files) {
     if (file.isDirectory()) {
-      yield* parseDirectory(basePath, [...currentPath, file.name])
+      yield* parseDirectory(basePath, [...currentPath, file.name]);
     } else {
       yield path.join(...currentPath, file.name);
     }
@@ -29,30 +29,31 @@ export class CreateManifest extends Command {
     const { args, flags } = this.parse(CreateManifest);
     if (flags.verbose) logger.level = 'debug';
 
-    const inputPath = path.resolve(args.inputFile)
+    const inputPath = path.resolve(args.inputFile);
     const stat = await fs.stat(inputPath);
     if (!stat.isDirectory()) {
-      logger.error({ path: inputPath }, "Base path is not a folder")
+      logger.error({ path: inputPath }, 'Base path is not a folder');
       return;
     }
 
     const manifest: Manifest = {
       path: inputPath,
       size: 0,
-      files: []
-    }
+      files: [],
+    };
 
     const pathReg = new RegExp('\\' + path.sep, 'g');
 
     for await (const fileName of parseDirectory(inputPath, [])) {
       const stat = await fs.stat(path.join(inputPath, fileName));
       manifest.size += stat.size;
-      manifest.files.push({ path: fileName, size: stat.size })
-      if (manifest.files.length % 1000 === 0) logger.debug({ count: manifest.files.length, path: fileName }, "Progress");
+      manifest.files.push({ path: fileName, size: stat.size });
+      if (manifest.files.length % 1000 === 0)
+        logger.debug({ count: manifest.files.length, path: fileName }, 'Progress');
     }
 
     const outputFile = args.inputFile.replace(pathReg, '_') + '.json';
     await fs.writeFile(outputFile, JSON.stringify(manifest, null, 2));
-    logger.info({ path: outputFile, count: manifest.files.length }, 'ManifestCreated')
+    logger.info({ path: outputFile, count: manifest.files.length }, 'ManifestCreated');
   }
 }
--- src/commands/sync.ts
+++ src/commands/sync.ts
@@ -1,21 +1,29 @@
-import Command, { flags } from "@oclif/command";
-import { logger } from "../log";
+import Command, { flags } from '@oclif/command';
+import { logger } from '../log';
 import { createReadStream, promises as fs } from 'fs';
-import { Manifest } from "../manifest";
-import pLimit from "p-limit";
-import * as path from "path";
+import { Manifest } from '../manifest';
+import pLimit from 'p-limit';
+import * as path from 'path';
 import S3 from 'aws-sdk/clients/s3';
 
 let client: S3;
 
-function headObject(ctx: { Bucket: string, Key: string }): Promise<S3.HeadObjectOutput | false> {
-  return client.headObject(ctx).promise().catch(e => {
-    if (e.code === 'NotFound') return false
-    throw e
-  })
+function headObject(ctx: { Bucket: string; Key: string }): Promise<S3.HeadObjectOutput | false> {
+  return client
+    .headObject(ctx)
+    .promise()
+    .catch((e) => {
+      if (e.code === 'NotFound') return false;
+      throw e;
+    });
 }
 
-async function findMostRecentUpload(mani: Manifest, bucket: string, prefix: string[], checkRange: number): Promise<number> {
+async function findMostRecentUpload(
+  mani: Manifest,
+  bucket: string,
+  prefix: string[],
+  checkRange: number,
+): Promise<number> {
   let foundUploaded = -1;
   let foundNotUploaded = -1;
   let count = 0;
@@ -23,23 +31,26 @@ async function findMostRecentUpload(mani: Manifest, bucket: string, prefix: stri
   let high = mani.files.length;
 
   while (low <= high) {
-    let mid = (low + high) >>> 1
+    const mid = (low + high) >>> 1;
     const file = mani.files[mid];
     const ctx = {
       Bucket: bucket,
-      Key: path.join(...prefix, file.path)
-    }
+      Key: path.join(...prefix, file.path),
+    };
 
     const found = await headObject(ctx);
     if (found) {
       if (mid > foundUploaded) foundUploaded = mid;
       low = mid + 1;
     } else {
-      if (mid < foundNotUploaded || foundNotUploaded == -1) foundNotUploaded = mid;
+      if (mid < foundNotUploaded || foundNotUploaded === -1) foundNotUploaded = mid;
       high = mid - 1;
     }
 
-    logger.info({ index: mid, path: ctx.Key, exists: found != false, foundUploaded, foundNotUploaded, low, high }, 'LastUpload:Search');
+    logger.info(
+      { index: mid, path: ctx.Key, exists: found !== false, foundUploaded, foundNotUploaded, low, high },
+      'LastUpload:Search',
+    );
     count++;
     if (count > 10) break;
   }
@@ -48,16 +59,18 @@ async function findMostRecentUpload(mani: Manifest, bucket: string, prefix: stri
     const file = mani.files[i];
     const ctx = {
       Bucket: bucket,
-      Key: path.join(...prefix, file.path)
-    }
-    const head = await headObject(ctx)
-    logger.debug({ index: i, path: ctx.Key, exists: head != true, sizeS3: head && head.ContentLength, sizeLocal: file.size }, 'LastUpload:SearchForComplete');
+      Key: path.join(...prefix, file.path),
+    };
+    const head = await headObject(ctx);
+    logger.debug(
+      { index: i, path: ctx.Key, exists: head !== true, sizeS3: head && head.ContentLength, sizeLocal: file.size },
+      'LastUpload:SearchForComplete',
+    );
 
-    if (head == false) return i;
+    if (head === false) return i;
     if (head.ContentLength !== file.size) return i;
-
   }
-  throw new Error('Failed')
+  throw new Error('Failed');
 }
 
 export class SnowballSync extends Command {
@@ -78,12 +91,12 @@ export class SnowballSync extends Command {
 
     const [bucket, ...prefix] = flags.target.slice(5).split('/');
 
-    const queue = pLimit(flags.concurrency)
+    const queue = pLimit(flags.concurrency);
 
     let endpoint = flags.endpoint;
     if (!endpoint.startsWith('http')) endpoint = 'http://' + endpoint + ':8080';
-    logger.info({ endpoint }, 'SettingS3 Endpoint')
-    client = new S3({ endpoint, s3ForcePathStyle:true })
+    logger.info({ endpoint }, 'SettingS3 Endpoint');
+    client = new S3({ endpoint, s3ForcePathStyle: true });
 
     if (!args.inputFile.endsWith('.json')) throw new Error('InputFile must be a json file');
 
@@ -95,18 +108,17 @@ export class SnowballSync extends Command {
     let promises = [];
     let lastFile = '';
 
-
-    const startIndex = await findMostRecentUpload(mani, bucket, prefix, flags.concurrency)
+    const startIndex = await findMostRecentUpload(mani, bucket, prefix, flags.concurrency);
 
     const logInterval = setInterval(() => {
       const mbMoved = ((size - lastSize) / 1024 / 1024).toFixed(2);
       lastSize = size;
       const percent = ((count / mani.files.length) * 100).toFixed(2);
-      logger.info({ count, percent, transferred: mbMoved, lastFile }, 'UploadProgress')
-    }, 2500)
+      logger.info({ count, percent, transferred: mbMoved, lastFile }, 'UploadProgress');
+    }, 2500);
     logInterval.unref();
 
-    logger.info({ startOffset: startIndex, files: mani.files.length }, 'Upload:Start')
+    logger.info({ startOffset: startIndex, files: mani.files.length }, 'Upload:Start');
     for (let index = startIndex; index < mani.files.length; index++) {
       const file = mani.files[index];
       const p = queue(async () => {
@@ -120,12 +132,15 @@ export class SnowballSync extends Command {
           Body: createReadStream(path.join(mani.path, file.path)),
         };
 
-        logger.debug({ index, path: file.path, size: file.size, target: `s3://${bucket}/${uploadCtx.Key}` }, 'Upload:Start')
+        logger.debug(
+          { index, path: file.path, size: file.size, target: `s3://${bucket}/${uploadCtx.Key}` },
+          'Upload:Start',
+        );
         await client.upload(uploadCtx).promise();
         // await new Promise(resolve => setTimeout(resolve, 100))
-      })
+      });
 
-      promises.push(p)
+      promises.push(p);
       if (promises.length > 250) {
         await Promise.all(promises);
         promises = [];
